1546777f41166d39c348f897efe1eeb3f0ac1989
Edgar <edgar@Edgars-MacBook-Pro.local>
2023-09-14 14:10:04 -0300

    chore(models): add annotations

--- backend/app/models/github_event.rb
+++ backend/app/models/github_event.rb
@@ -1,2 +1,13 @@
 class GithubEvent < ApplicationRecord
 end
+
+# == Schema Information
+#
+# Table name: github_events
+#
+#  id         :integer          not null, primary key
+#  event_type :string
+#  payload    :json
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
--- backend/test/models/github_event_test.rb
+++ backend/test/models/github_event_test.rb
@@ -5,3 +5,14 @@ class GithubEventTest < ActiveSupport::TestCase
   #   assert true
   # end
 end
+
+# == Schema Information
+#
+# Table name: github_events
+#
+#  id         :integer          not null, primary key
+#  event_type :string
+#  payload    :json
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
